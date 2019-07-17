34f7520149f5e75417e157ce68d3235a442431d0
SenseiFavour <fchukwuedo@gmail.com>
2019-07-18 00:34:29 +0100

    Update config.php

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -23,4 +23,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://fountainpay.herokuapp.com/';
